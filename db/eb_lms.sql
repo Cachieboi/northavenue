150207c8a29cc200c297f4629b04fb001669e829
Cachieboi <70936283+Cachieboi@users.noreply.github.com>
2021-12-03 20:52:15 +0800

    first

---
+++ db/eb_lms.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2014 at 03:09 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `eb_lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_title` varchar(100) NOT NULL,
+  `category_id` int(50) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `book_copies` int(11) NOT NULL,
+  `book_pub` varchar(100) NOT NULL,
+  `publisher_name` varchar(100) NOT NULL,
+  `isbn` varchar(50) NOT NULL,
+  `copyright_year` int(11) NOT NULL,
+  `date_receive` varchar(20) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `status` varchar(30) NOT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+--
+-- Dumping data for table `book`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE IF NOT EXISTS `borrow` (
+  `borrow_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` bigint(50) NOT NULL,
+  `date_borrow` varchar(100) NOT NULL,
+  `due_date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`borrow_id`),
+  KEY `borrowerid` (`member_id`),
+  KEY `borrowid` (`borrow_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=485 ;
+
+--
+-- Dumping data for table `borrow`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowdetails`
+--
+
+CREATE TABLE IF NOT EXISTS `borrowdetails` (
+  `borrow_details_id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` int(11) NOT NULL,
+  `borrow_id` int(11) NOT NULL,
+  `borrow_status` varchar(50) NOT NULL,
+  `date_return` varchar(100) NOT NULL,
+  `book_fine` int(11) NOT NULL,
+  PRIMARY KEY (`borrow_details_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=165 ;
+
+--
+-- Dumping data for table `borrowdetails`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `classname` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_id` (`category_id`),
+  KEY `classid` (`category_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=801 ;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `classname`) VALUES
+(1, 'Periodical'),
+(2, 'English'),
+(3, 'Math'),
+(4, 'Science'),
+(5, 'Encyclopedia'),
+(6, 'Filipiniana'),
+(7, 'Newspaper'),
+(8, 'General'),
+(9, 'References');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lost_book`
+--
+
+CREATE TABLE IF NOT EXISTS `lost_book` (
+  `Book_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ISBN` int(11) NOT NULL,
+  `Member_No` varchar(50) NOT NULL,
+  `Date Lost` date NOT NULL,
+  PRIMARY KEY (`Book_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE IF NOT EXISTS `member` (
+  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `year_level` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
+
+--
+-- Dumping data for table `member`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE IF NOT EXISTS `type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `borrowertype` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `borrowertype` (`borrowertype`),
+  KEY `id` (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=42 ;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `borrowertype`) VALUES
+(2, 'Teacher'),
+(20, 'Employee'),
+(21, 'Non-Teaching'),
+(22, 'Student'),
+(32, 'Contruction');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(2, 'admin', 'admin', 'john', 'smith');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
